3f513a3f911399c1ba6c090c4d5eb22e17147f7f
Patti85 <pbeier24@googlemail.com>
2021-01-03 13:54:07 +0100

    write all titles uppercase

--- uppercase-titles.php
+++ uppercase-titles.php
@@ -11,45 +11,12 @@
  */
 
 
-function hello_world()
+function write_all_titles_uppercase($post)
 {
-    return "Hello World";
+    $post->post_title = strtoupper($post->post_title);    
 }
-add_shortcode('write_hello_world', 'hello_world');
 
+add_filter('the_post', 'write_all_titles_uppercase'); 
 
-function get_first_title()
-{
-    global $loop;
-    $all_posts = get_posts(array('numberposts' => -1));
-    $loop = new WP_Query($all_posts);
-    if ($loop->have_posts()) {
-        while ($loop->have_posts()) {
-            $loop->the_post();
-            return the_title();
-        }
-    }
-    return "no posts";
-}
-add_shortcode('write_first_title', 'get_first_title');
-
-function write_all_titles_uppercase()
-{
-    $arg = new WP_Query( array( 'numberposts' => -1) ); 
-    $a = "";
-    while ( $arg->have_posts()) : $arg->the_post();
-     //the_title('<h1>', '</h1>', true); 
-     $a = $a . strtoupper(get_the_title());
-    endwhile;
-    return $a;
-   
-}
-add_shortcode('write_all_posts', 'write_all_titles_uppercase');
-
-function make_all_titles_hello($post)
-{
-    $post->post_title = 'HELLO'; 
-}
-add_filter('the_post', 'make_all_titles_hello');
 
 defined('ABSPATH') or die("Thanks for visting");
